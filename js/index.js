f3261f3653b8b5e4cd87dbd231ef833fc25bb4ab
DoriZuu48 <dori.zuu48@gmail.com>
2021-08-26 21:32:42 +0300

    2

--- js/index.js
+++ js/index.js
@@ -1,33 +1,3 @@
-// let products = [
-// {id: 1, 
-// 		Xiaomi: {
-// 				name: 'Xiaomi 365',
-// 				id: '#21433254354532',
-// 				desc: 'Color: White // Extra: Sport Tire + Battery High 100',
-// },
-// 		Price: {
-// 				opt: '+50.99€ Sport Tire',
-// 				sub: '+35€ Battery High',
-// 				price: '488.99€'
-// },
-// 		TotalPrice: {
-// 				count: '488.99€',
-// 		}
-// 	}
-// ]
-
-// let cart = []
-// document.addEventListener("DOMContentLoaded", function() { 
-//   fillStorageTable(products);
-// });
-
-
-
-// const app = document.querySelector('.container');
-// const template = document.querySelector('#template');
-
-
-
 
 let countInput;
 let totalDiv;
@@ -68,12 +38,6 @@ const calculateTotal = () => {
 
 document.getElementById("button1").style.backgroundColor ="#E2E6E9";
 document.getElementById("button2").style.backgroundColor ="#E2E6E9";
-
-
-document.getElementById("op1").style.color = "#626364";
-document.getElementById("op2").style.color = "#626364";
-
-
 document.getElementById("button1").onclick = function(){
     this.style.backgroundColor = "#F05359";
     document.getElementById("button2").style.backgroundColor ="#E2E6E9";
