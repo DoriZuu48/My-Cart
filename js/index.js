5a3674be182a3c3b4d4b302871683fb1228833e3
DoriZuu48 <dori.zuu48@gmail.com>
2021-09-02 00:25:34 +0300

    q

--- js/index.js
+++ js/index.js
@@ -1,57 +1,55 @@
 
 function decrement(e) {
-    const btn = e.target.parentNode.parentElement.querySelector(
-      'button[data-action="decrement"]'
-    );
-    const target = btn.nextElementSibling;
-    let value = Number(target.value);
-    value--;
-    target.value = value;
-  }
-
-  function increment(e) {
-    const btn = e.target.parentNode.parentElement.querySelector(
-      'button[data-action="decrement"]'
-    );
-    const target = btn.nextElementSibling;
-    let value = Number(target.value);
-    value++;
-    target.value = value;
-  }
-
-  const decrementButtons = document.querySelectorAll(
-    `button[data-action="decrement"]`
+  const btn = e.target.parentNode.parentElement.querySelector(
+    'button[data-action="decrement"]'
   );
-
-  const incrementButtons = document.querySelectorAll(
-    `button[data-action="increment"]`
-  );
-
-  decrementButtons.forEach(btn => {
-    btn.addEventListener("click", decrement);
-  });
-
-  incrementButtons.forEach(btn => {
-    btn.addEventListener("click", increment);
-  });
-
-
-
-
-
-document.getElementById("button1").style.backgroundColor ="#edf1f4";
-document.getElementById("button2").style.backgroundColor ="#edf1f4";
-
-document.getElementById("button1").onclick = function(){
-    this.style.backgroundColor = "#fb3d46";
-    document.getElementById("button2").style.backgroundColor ="#edf1f4";
-
-
+  const target = btn.nextElementSibling;
+  let value = Number(target.value);
+  value--;
+  target.value = value;
 }
-document.getElementById("button2").onclick = function(){
-    this.style.backgroundColor ="#fb3d46";
-    document.getElementById("button1").style.backgroundColor ="#edf1f4";
 
-};
+function increment(e) {
+  const btn = e.target.parentNode.parentElement.querySelector(
+    'button[data-action="decrement"]'
+  );
+  const target = btn.nextElementSibling;
+  let value = Number(target.value);
+  value++;
+  target.value = value;
+}
+
+const decrementButtons = document.querySelectorAll(
+  `button[data-action="decrement"]`
+);
+
+const incrementButtons = document.querySelectorAll(
+  `button[data-action="increment"]`
+);
+
+decrementButtons.forEach(btn => {
+  btn.addEventListener("click", decrement);
+});
+
+incrementButtons.forEach(btn => {
+  btn.addEventListener("click", increment);
+});
 
 
+
+
+
+// document.getElementById("button1").style.backgroundColor ="#edf1f4";
+// document.getElementById("button2").style.backgroundColor ="#edf1f4";
+
+// document.getElementById("button1").onclick = function(){
+//   this.style.backgroundColor = "#fb3d46";
+//   document.getElementById("button2").style.backgroundColor ="#edf1f4";
+
+
+// }
+// document.getElementById("button2").onclick = function(){
+//   this.style.backgroundColor ="#fb3d46";
+//   document.getElementById("button1").style.backgroundColor ="#edf1f4";
+
+// };
